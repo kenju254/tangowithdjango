cd136a3c4b8871dc588894830ba54df3c93c5502
kenju254 <kenmbugua@gmail.com>
2014-07-24 19:27:11 +0300

    initial commit

---
+++ rango/views.py
@@ -0,0 +1,25 @@
+# Create your views here.
+from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
+from rango.models import Category
+
+def index(request):
+    #We are requesting the context of the request.
+    #This context contains information such as the client's machine details and metadata
+    context = RequestContext(request)
+
+    #What we do is we construct a dictionary to pass to the template engine as its context
+    #Note the key boldmessage is the same as {{ boldmessage }} in the template!
+
+    category_list = Category.objects.all()
+    context_dict = {'categories' : category_list}
+
+    #Return a rendered response to send to the client.
+    #We make use of the shortcut function to make our lives easier.
+    #Note that the first parameter is the template we wish to use.
+    return render_to_response('rango/index.html', context_dict, context)
+
+def about(request):
+    return render_to_response('rango/about.html')
