a0ce41ad65e804eb7afce9b76dcbd1f795789605
kenju254 <kenmbugua@gmail.com>
2014-07-28 17:37:48 +0300

    Updated the category and add_page view

--- rango/views.py
+++ rango/views.py
@@ -3,8 +3,47 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
-from rango.models import Category
-from rango.forms import CategoryForm
+from rango.models import Category, Page
+from rango.forms import CategoryForm , PageForm , UserForm, UserProfileForm
+
+
+def encode_url(str):
+    return str.replace('','_')
+
+def decode_url(str):
+    return str.replace('_', '')
+
+def register(request):
+    context = RequestContext(request)
+
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+
+            user.set_password(user.password)
+
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
+            profile.save()
+
+            registered = True
+        else:
+            print user_form.errors , profile_form.errors
+
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
 
 def index(request):
     #We are requesting the context of the request.
@@ -21,6 +60,33 @@ def index(request):
     #We make use of the shortcut function to make our lives easier.
     #Note that the first parameter is the template we wish to use.
     return render_to_response('rango/index.html', context_dict, context)
+
+
+
+def category(request, category_name_url):
+
+    context = RequestContext(request)
+
+    category_name = decode_url(category_name_url)
+
+    context_dict = {'category_name' : category_name,
+                    'category_name_url': category_name_url}
+
+    category_list = Category.objects.order_by('likes')[:5]
+    context_dict = {'categories': category_list}
+
+    try:
+        category = Category.objects.get(name__iexact=category_name)
+        context_dict['category'] = category
+
+        pages = Page.objects.filter(category=category).order_by('views')
+
+        context_dict['pages'] = pages
+
+    except Category.DoesNotExist:
+        pass
+
+    return render_to_response('rango/category.html', context_dict, context)
 
 def add_category(request):
 
@@ -41,6 +107,44 @@ def add_category(request):
         form = CategoryForm()
 
     return render_to_response('rango/add_category.html', {'form': form}, context)
+
+def add_page(request, category_name_url):
+    context = RequestContext(request)
+
+    category_name = decode_url(category_name_url)
+    if request.method == "POST":
+        form = PageForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=False)
+
+            try:
+                cat = Category.objects.get(name=category_name)
+                page.create = cat
+            except Category.DoesNotExist:
+                return render_to_response('rango/add_category.html', {}, context)
+
+            page.views = 0
+
+            page.save()
+
+            return category(request, category_name_url)
+
+        else:
+            print form.errors
+
+    else:
+        form = PageForm()
+
+    return render_to_response('rango/add_page.html',
+        {'category_name_url': category_name_url,
+        'category_name':category_name,'form':form},
+        context)
+
+
+
+
+
 
 def about(request):
     return render_to_response('rango/about.html')
